ef2e5b604d50fa74a19d3e36d898112e6b5c9928
plchao <poseidon31418926.cs06@g2.nctu.edu.tw>
2022-06-07 19:49:01 +0800

    [add 500 test case]

--- src/apcService/utilities/__tests__/process.test.js
+++ src/apcService/utilities/__tests__/process.test.js
@@ -30,7 +30,31 @@ describe('test', ()=>{
             }
         });
         require('../../routers/v1/process');
-        // i dont know why 500 not 200 XD
         expect(mRes.status).toBeCalledWith(200);
+    })
+    it('Call by 500', ()=>{
+        global.cache = null;
+        const express = require('express');
+        const mRouter = {post: jest.fn()};
+        jest.spyOn(express, 'Router').mockImplementationOnce(() => mRouter);
+        const mReq = {body: jest.fn().mockReturnThis({
+            id:0,
+            type:'SHARON',
+            thickness:0.1,
+            moisture:0.2
+        })};
+        const mSend = jest.fn().mockReturnThis(500);
+        // Problem might happened here, but have no idea
+        const mRes = { 
+            status: jest.fn().mockReturnValue({send:mSend}),
+            render: jest.fn(tFactor=0.1, mFactor=0.2, metaData=null) 
+        };
+        mRouter.post.mockImplementation((path, callback) => {
+            if (path === '/api/v1/process') {
+              callback(mReq, mRes);
+            }
+        });
+        require('../../routers/v1/process');
+        expect(mRes.status).toBeCalledWith(500);
     })
 })
